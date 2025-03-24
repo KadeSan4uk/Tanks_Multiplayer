1bd94f3abcfdabd3370b89b19884cea674e0a0e6
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-03-24 23:15:40 +0200

    Coin Spawner

---
+++ Assets/Scripts/Core/Coins/CoinSpawner.cs
@@ -0,0 +1,60 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class CoinSpawner : NetworkBehaviour
+{
+    [SerializeField] private RespawningCoin _coinPrefab;
+    [SerializeField] private int _maxCoin = 50;
+    [SerializeField] private int _coinValue = 5;
+    [SerializeField] private Vector2 _xSpawnRange;
+    [SerializeField] private Vector2 _ySpawnRange;
+    [SerializeField] private LayerMask _layerMask;
+
+    private float coinRadius;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) { return; }
+        coinRadius = _coinPrefab.GetComponent<CircleCollider2D>().radius;
+
+        for (int i = 0; i < _maxCoin; i++)
+        {
+            SpawnCoin();
+        }
+    }
+
+    private void SpawnCoin()
+    {
+        RespawningCoin coinInstance = Instantiate(_coinPrefab, GetSpawnPoint(), Quaternion.identity);
+
+        coinInstance.SetValue(_coinValue);
+        coinInstance.GetComponent<NetworkObject>().Spawn();
+
+        coinInstance.OnCollected += HandleCoinCollected;
+    }
+
+    private void HandleCoinCollected(RespawningCoin coin)
+    {
+        coin.transform.position = GetSpawnPoint();
+        coin.Reset();
+    }
+
+    private Vector2 GetSpawnPoint()
+    {
+        const int maxAttempts = 10;
+        for (int i = 0; i < maxAttempts; i++)
+        {
+            float x = Random.Range(_xSpawnRange.x, _xSpawnRange.y);
+            float y = Random.Range(_ySpawnRange.x, _ySpawnRange.y);
+            Vector2 spawnPoint = new Vector2(x, y);
+
+            Collider2D trySpawn = Physics2D.OverlapCircle(spawnPoint, coinRadius, _layerMask);
+            if (trySpawn == null)
+            {
+                return spawnPoint;
+            }
+        }
+
+        return new Vector2(Random.Range(_xSpawnRange.x, _xSpawnRange.y), Random.Range(_ySpawnRange.x, _ySpawnRange.y));
+    }
+}
--- Assets/Scripts/Core/Coins/RespawningCoin.cs
+++ Assets/Scripts/Core/Coins/RespawningCoin.cs
@@ -1,7 +1,26 @@
+using System;
 using UnityEngine;
 
 public class RespawningCoin : Coin
 {
+    public Action<RespawningCoin> OnCollected;
+
+    private Vector3 _previousPosition;
+
+    private void Update()
+    {
+        CheckPreviousPosition();
+    }
+
+    private void CheckPreviousPosition()
+    {
+        if (_previousPosition != transform.position)
+        {
+            Show(true);
+            _previousPosition = transform.position;
+        }
+    }
+
     public override int Collect()
     {
         if (!IsServer)
@@ -14,6 +33,13 @@ public class RespawningCoin : Coin
 
         alreadyCollected = true;
 
+        OnCollected?.Invoke(this);
+
         return coinValue;
+    }
+
+    public void Reset()
+    {
+        alreadyCollected = false;
     }
 }
