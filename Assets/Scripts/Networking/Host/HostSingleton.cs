80bad3e2ea940f7b7414151dc72a9618410c6292
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-03-25 21:40:37 +0200

    Application Controller

---
+++ Assets/Scripts/Networking/Host/HostSingleton.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HostSingleton : MonoBehaviour
+{
+    private static HostSingleton _instance;
+    private HostGameManager _gameManager;
+
+    public static HostSingleton Instance
+    {
+        get
+        {
+            if (_instance is not null) { return _instance; }
+
+            _instance = FindFirstObjectByType<HostSingleton>();
+
+            if (_instance == null)
+            {
+                Debug.LogError("No HostSingleton in the scene!");
+                return null;
+            }
+            return _instance;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+
+    }
+
+    public void CreateHost()
+    {
+        _gameManager = new HostGameManager();
+    }
+}
