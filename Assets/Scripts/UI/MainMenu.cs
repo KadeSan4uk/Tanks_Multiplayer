3883445c23894b842c93ca716bd416a9b82da5fb
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-04-22 15:13:43 +0300

    Finish Project

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,12 +6,20 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_InputField _joinCodeField;
     [SerializeField] private Button _enterCodeButton;
+    [SerializeField] private Button _enterConnectButton;
+
+    private const int RequiredCodeLength = 6;
 
     private void Start()
     {
         if (ClientSingleton.Instance == null) { return; }
 
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        EnterCodeVerification();
     }
 
     public async void StartHost()
@@ -23,8 +31,8 @@ public class MainMenu : MonoBehaviour
     {
         _enterCodeButton.gameObject.SetActive(false);
         _joinCodeField.gameObject.SetActive(true);
+        _enterConnectButton.gameObject.SetActive(true);
         _joinCodeField.Select();
-
     }
 
     public async void StartClient()
@@ -44,4 +52,24 @@ public class MainMenu : MonoBehaviour
         await ClientSingleton.Instance.GameManager.StartClientAsync(_joinCodeField.text);
     }
 
+    private void EnterCodeVerification()
+    {
+        if (!_joinCodeField) { return; }
+
+        if (_joinCodeField.enabled)
+        {
+            if (_joinCodeField.text.Length == RequiredCodeLength && Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                StartClient();
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid number of characters.");
+        }
+        if (_enterConnectButton)
+        {
+            _enterConnectButton.interactable = _joinCodeField.text.Length == RequiredCodeLength;
+        }
+    }
 }
